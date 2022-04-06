bee47826a1a71da9f15564ab3bb6feabd7907130
TiagoSenaD <50551963+TiagoSenaD@users.noreply.github.com>
2022-04-06 10:33:50 -0300

    small updates

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#TODO: Make its works
+
+# Terminate already running bar instances
+# pgrep polybar | xargs kill
+# Best way 
+kill -9 $(pgrep -f 'polybar') >/dev/null 2>&1
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#launch mybar
+
+polybar -c ./config.ini My_polybar &
+echo "Bars launched..."
